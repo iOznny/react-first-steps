eb9ab666d348899929e520abd6957a8d41b02ed4
iOznny <f000170014@gmail.com>
2021-07-11 14:30:19 -0500

    Cambios

---
+++ src/FirstApp.js
@@ -0,0 +1,11 @@
+// Los componentes existen dos tipos por clases y por funciones.
+
+import React from 'react';
+
+
+// Functional Components
+const FisrtApp = () => {
+    return <h1>Hola Mundo</h1>;
+}
+
+export default FisrtApp;
